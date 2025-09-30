46c3ad7b5fbdee8d6a135c82510f25b11c60e338
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-09-30 11:52:25 +0200

    fix : https://github.com/NoanBregeon/Projet_1/issues/2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,18 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="../css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
     @yield('styles')
-</head>
-<body class="bg-light">
-    <div class="container py-4">
-        @yield('content')
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
 </head>
 <body class="bg-light">
     <div class="container py-4">
